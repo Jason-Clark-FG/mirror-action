0ccf5afd4867251b275e675350a427eaa713d846
Jason Clark <jason.e.clark@gmail.com>
2025-03-15 15:39:50 -0400

    chore: Update entrypoint.sh to batch multiple branches, update docker image base
    [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM alpine/git@sha256:ec76d75a4b5367f16cf6dc859e23c06656761ad4dfcb1716c1800582ce05f5e8
+# FROM alpine/git@sha256:ec76d75a4b5367f16cf6dc859e23c06656761ad4dfcb1716c1800582ce05f5e8
+FROM alpine/git@sha256:c818699b2927abbaacbbc5e5a593646989588c9b7f321055f6e571da140e43fe
 
 RUN apk --no-cache add bash
 
@@ -7,10 +8,9 @@ LABEL "com.github.actions.description"="Automate mirroring of git commits to ano
 LABEL "com.github.actions.icon"="git-commit"
 LABEL "com.github.actions.color"="green"
 
-LABEL "repository"="https://github.com/yesolutions/mirror-action"
-LABEL "homepage"="https://github.com/yesolutions/mirror-action"
-LABEL "maintainer"="Spencer Phillip Young <spencer.young@spyoung.com>"
-
+LABEL "repository"="https://github.com/Jason-Clark-FG/mirror-action"
+LABEL "homepage"="https://github.com/Jason-Clark-FG/mirror-action"
+LABEL "maintainer"="Jason Clark <jclark@factorsgroup.com>"
 
 COPY entrypoint.sh /entrypoint.sh
 COPY cred-helper.sh /cred-helper.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,12 @@ REMOTE_NAME=${INPUT_REMOTE_NAME:-"mirror"}
 GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
 GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
 GIT_REF=${INPUT_GIT_REF}
-GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
+GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--force --prune"}
 GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
 GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
 HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2>/dev/null || /bin/true)"
+BRANCH_BATCH_SIZE=${INPUT_BRANCH_BATCH_SIZE:-50}
+PUSH_TAGS=${INPUT_PUSH_TAGS:-true}
 
 if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
     echo "WARNING: repo not checked out; attempting checkout" > /dev/stderr
@@ -28,14 +30,13 @@ if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
         echo "WARNING: SRC_REPO env variable not defined" > /dev/stderr
         SRC_REPO="https://github.com/${GITHUB_REPOSITORY}.git" > /dev/stderr
         echo "Assuming source repo is ${SRC_REPO}" > /dev/stderr
-     fi
+    fi
     git init > /dev/null
     git remote add origin "${SRC_REPO}"
     git fetch --all > /dev/null 2>&1
 fi
 
 git config --global credential.username "${GIT_USERNAME}"
-
 
 if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
     mkdir -p ~/.ssh
@@ -47,17 +48,17 @@ if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
     fi
     chmod 600 ~/.ssh/id_rsa
     if [[ "${GIT_SSH_KNOWN_HOSTS}" != "" ]]; then
-      echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
-      git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
+        echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
+        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
     else
-      if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
-        echo "WARNING: no known_hosts set and host verification is enabled (the default)"
-        echo "WARNING: this job will fail due to host verification issues"
-        echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
-        exit 1
-      else
-        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-      fi
+        if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
+            echo "WARNING: no known_hosts set and host verification is enabled (the default)"
+            echo "WARNING: this job will fail due to host verification issues"
+            echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
+            exit 1
+        else
+            git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+        fi
     fi
 else
     git config --global core.askPass /cred-helper.sh
@@ -65,8 +66,25 @@ else
 fi
 
 git remote add ${REMOTE_NAME} "${REMOTE}"
+
 if [[ "${INPUT_PUSH_ALL_REFS}" != "false" ]]; then
-    eval git push ${GIT_PUSH_ARGS} ${REMOTE_NAME} "\"refs/remotes/origin/*:refs/heads/*\""
+    # Batch pushing logic
+    remote_branches=$(git branch -r | grep origin/ | awk '{print $1}' | sed 's/origin\///')
+
+    for i in $(seq 0 $((${#remote_branches[@]} - 1))); do
+        branch=${remote_branches[$i]}
+        if (( $i % $BRANCH_BATCH_SIZE == 0 )); then
+            batch=()
+        fi
+        batch+=("refs/remotes/origin/$branch:refs/heads/$branch")
+        if (( $i % $BRANCH_BATCH_SIZE == $(($BRANCH_BATCH_SIZE - 1)) || $i == $((${#remote_branches[@]} - 1)) )); then
+            eval git push ${GIT_PUSH_ARGS} ${REMOTE_NAME} "${batch[@]}"
+        fi
+    done
+    # Push tags separately after branches, if configured.
+    if [[ "${PUSH_TAGS}" == "true" ]]; then
+        eval git push ${GIT_PUSH_ARGS} ${REMOTE_NAME} --tags
+    fi
 else
     if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
         echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch" > /dev/stderr
